ccb60a4d583292c16875c5d8d35f7efbd178a225
Alfa-Design <john7ouma@gmail.com>
2023-02-16 14:19:43 -0800

    a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+/*
+ * file: 5-printf.c
+ * Auth: John Ouma Otieno <john7ouma@gmail.com>
+ */
+#include <stdio.h>
+
+/*
+ * main - prints "with proper grammar, but the outcome is a piece of art,"
+ * retirns 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
+}
