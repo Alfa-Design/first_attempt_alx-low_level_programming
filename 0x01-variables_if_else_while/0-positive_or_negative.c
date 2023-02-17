7dd6666e462a2553c143c466fcd27115991f7f64
Alfa-Design <john7ouma@gmail.com>
2023-02-17 03:01:13 -0800

    Prints a random number and states whether  it is positive, negative, or zero.

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,3 +1,26 @@
+/*
+ * File: 0-positive_or_negative.c
+ * Auth: John Ouma otieno <john7ouma@gmail.com>
+ */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0.
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
 if (n > 0)
 {
 	printf("%d  is a positive number. \n", n);
@@ -9,3 +32,6 @@ else {
 	printf("%d is negative. \n", n);
 }
 
+return (0);
+
+}
