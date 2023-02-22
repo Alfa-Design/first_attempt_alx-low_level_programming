979c6af7e3fd5a24498bc535254869b84c64ae39
Alfa-Design <john7ouma@gmail.com>
2023-02-21 23:49:28 -0800

    a program that prints _putchar, followed by a new line.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+/*
+ * File: 0-putchar.c
+ * Auth: John Ouma Otieno <john7ouma@gmail.com>
+ */
+
+
+#include < "main.h">
+
+/**
+ * a program that prints _putchar, followed by a new line.
+ *
+ * Return: On success 0.
+ * 
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+
+	return (0);
+}
