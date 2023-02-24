ade8639844834e0eaf9e11e349b12938fb09e1e3
Alfa-Design <john7ouma@gmail.com>
2023-02-24 15:33:45 -0800

    print an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,17 +6,6 @@
  */
 void print_number(int n)
 {
-	if (n < 0) {
-        putchar('-');
-        n = -n;
-    }
-
-    if (n == 0)
-        putchar('0');
-
-    if (n/10)
-        printnumber(n/10);
-
-    putchar(n%10 + '0');
+	_putchar('n');
 	return (0);
 }
