7d73acbae20f52354f75c7946b44c2af077199c5
Alfa-Design <john7ouma@gmail.com>
2023-02-27 18:15:08 -0800

    password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,73 +9,38 @@
  *
  * Return - a password
  *
- *
  */
 
-void randomPasswordGeneration(int N)
+#define MIN_CHAR 8
+#define MAX_CHAR 1023
+#define BUF_SIZE 1024
+#define PRIME_MOD 937
+
+int main (int argc, char **argv)
 {
-	int i = 0;
+    int N;
+    char password [BUF_SIZE];
 
-        int randomizer = 0;
-
-    // Seed the random-number generator
-    // with current time so that the
-    // numbers will be different every time
-    srand((unsigned int)(time(NULL)));
-
-    // Array of numbers
-    char numbers[] = "0123456789";
-
-    // Array of small alphabets
-    char letter[] = "abcdefghijklmnoqprstuvwyzx";
-
-    // Array of capital alphabets
-    char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
-
-    // Array of all the special symbols
-    char symbols[] = "!@#$^&*?";
-
-    // Stores the random password
-    char password[N];
-
-    // To select the randomizer
-    // inside the loop
-    randomizer = rand() % 4;
-
-    // Iterate over the range [0, N]
-    for (i = 0; i < N; i++) {
-
-        if (randomizer == 1) {
-            password[i] = numbers[rand() % 10];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
+    while (1) {
+        // get N
+        while (1) {
+            printf ("Enter number of digits: ");
+            scanf ("%d", &N);
+            if (!N) exit (EXIT_SUCCESS);
+            if (N >= MIN_CHAR && N <= MAX_CHAR)
+                break;
+            printf ("Password should be between %d - %d characters\n", MIN_CHAR, MAX_CHAR);
         }
-        else if (randomizer == 2) {
-            password[i] = symbols[rand() % 8];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-        else if (randomizer == 3) {
-            password[i] = LETTER[rand() % 26];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-        else {
-            password[i] = letter[rand() % 26];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
+
+        time_t now = time (NULL);
+
+        srand ((unsigned int) (now % PRIME_MOD));
+
+        for (int i = 0; i < N; i++)
+            password [i] = 33 + rand () % 94;
+
+        password [N] = '\0';
+
+        printf ("password = %s\n", password);
     }
-}
-
-int main()
-{
-    // Length of the password to
-    // be generated
-    int N = 9;
-
-    // Function Call
-    randomPasswordGeneration(N);
-
-    return 0;
 }
