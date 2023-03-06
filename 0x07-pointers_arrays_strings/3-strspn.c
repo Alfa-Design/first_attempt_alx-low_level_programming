6aee21962016a5da23ede4b3e314d91abea1d355
Alfa-Design <john7ouma@gmail.com>
2023-03-06 15:30:38 -0800

    a correction

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,25 +10,24 @@
  * of bytes from @accept
  */
 
-
 unsigned int _strspn(char *s, char *accept)
 {
 	int i, j;
 	int c = 0;
 
-	for(i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if(s[i] != 32}
-		{ 
-			for(j =0; accept[j] != '\0'; j++
+		if (s[i] != 32)
+		{
+			for (j = 0; accept[j] != '\0'; j++)
 			{
-				if(s[i] == accept[i]
-				c++;
+				if (s[i] == accept[j])
+					c++;
 			}
 		}
 		else
+			return (c);
 
-		return(c);
 	}
-	return(c);
+	return (c);
 }
