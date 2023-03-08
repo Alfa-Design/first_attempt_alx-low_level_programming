ad0d8ec72c9aa14da404af4903b3ebe36ef5059e
Alfa-Design <john7ouma@gmail.com>
2023-03-08 02:08:15 -0800

    compare two strings

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
 /**
- * is_palindrome - check if a string is a palindrome.
+ * is_palindrome - check if a string is a palindrome
  * @s: char array string
  * Return: 1 if palindrome, 0 if not
  */
 
-int is-palindrome(char *s)
+int is_palindrome(char *s)
 {
 	int length;
+
 	length = get_length(s) - 1;
-	
+
 	return (my_pal(s, --length));
-
-
 }
 
 /**
@@ -28,7 +27,6 @@ int get_length(char *s)
 		return (1);
 	else
 		return (1 + get_length(++s));
-
 }
 
 /**
@@ -38,16 +36,15 @@ int get_length(char *s)
  * Return: 1 if palindrome, 0 if not
  */
 
-int my_pal(char *s, int 1)
+int my_pal(char *s, int l)
 {
-	if (*s == *(s + 1)
+	if (*s == *(s + l))
 	{
-		if(1 <= 0)
-		       return (1);
+		if (l <= 0)
+			return (1);
 		else
-			return (my_pal(++s, 1 - 2));
+			return (my_pal(++s, l - 2));
 	}
 	else
 		return (0);
-
 }
