bbf70b6c2d358d2458237f5a7708c1f77d100e82
Alfa-Design <john7ouma@gmail.com>
2023-03-08 00:13:19 -0800

    Gives x power y

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,12 +8,12 @@
 
 int factorial(int n)
 {
-	if(n < 0)
-		return(-1);
-	else if(n == 1)
-		return(n);
+	if (n < 0)
+		return (-1);
+	else if (n == 1)
+		return (n);
 	else 
 	{
-		return(n * factorial(n-1));
+		return (n * factorial(n-1));
 	}
 }
