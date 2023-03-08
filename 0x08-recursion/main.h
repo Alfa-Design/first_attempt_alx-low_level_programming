3d68c1f14fe3f7f81da712cf7d3cee63922dadb8
Alfa-Design <john7ouma@gmail.com>
2023-03-08 02:15:48 -0800

    main update

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,3 +10,13 @@ int _strlen_recursion(char *s);
 void _print_rev_recursion(char *s);
 void _puts_recursion(char *s);
 int _putchar(char c);
+int halp(int c, int i);
+int is_prime_number(int n);
+int hai(int n, int d);
+int _sqrt(int x, int i);
+int is_palindrome(char *s);
+int get_length(char *s);
+int my_pal(char *s, int l);
+int wildcmp(char *s1, char *s2);
+int findsrc(char *s, char c, int i, int p);
+
