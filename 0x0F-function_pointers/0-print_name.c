4644fbf81a9d44273792720f94e81a7cfdc5f214
Alfa-Design <john7ouma@gmail.com>
2023-03-22 07:54:58 -0700

    third trial of printing a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -8,6 +8,8 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if(name != NULL && name != NULL)
-	f(name);
+	if (name != NULL && f != NULL)
+	{
+		f(name);
+	}
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,10 +1,8 @@
 #ifndef FUNCTION_POINTER_H
-
 #define FUNCTION_POINTERS_H
 #include <stddef.h>
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-_putchar.c
 
 #endif /* The prototypes of all your functions and the prototype of the function _putchar */
