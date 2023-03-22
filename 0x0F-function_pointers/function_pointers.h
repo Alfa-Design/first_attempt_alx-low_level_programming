0b91d935245b09d039c4b4263e0fa3ef795812c3
Alfa-Design <john7ouma@gmail.com>
2023-03-22 08:08:56 -0700

    Trying to rectify the undefined  main error

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,16 @@
-#ifndef FUNCTION_POINTER_H
-#define FUNCTION_POINTERS_H
-#include <stddef.h>
-void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
+#ifndef FUNCTION_POINTERS_H
+	#define FUNCTION_POINTERS_H
 
-#endif /* The prototypes of all your functions and the prototype of the function _putchar */
+
+	#include <stddef.h>
+	#include <stdlib.h>
+
+
+	void print_name(char *name, void (*f)(char *));
+	int _putchar(char c);
+	void array_iterator(int *array, size_t size, void (*action)(int));
+	int int_index(int *array, int size, int (*cmp)(int));
+
+
+#endif
+
