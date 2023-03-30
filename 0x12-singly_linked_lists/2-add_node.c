3edeaed417558c7654ae24b7ab348422c6521a78
Alfa-Design <john7ouma@gmail.com>
2023-03-30 11:32:59 -0700

    debugging add_node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,18 +11,21 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new;
-	unsigned int len = 0;
+ list_t *new;
+ unsigned int len = 0;
 
-	while (str[len])
-		len++;
-	new = malloc(sizeoff(list_t));
-	if (new)
-		return (NULL);
-	new->str = strup(str);
-	new->len = len;
-	new->next = (*head);
-	(*head) = new;
+ while (str[len])
+ len++;
 
-	return (*head);
+ new = malloc(sizeof(list_t));
+ if (!new)
+ return (NULL);
+
+ new->str = strdup(str);
+ new->len = len;
+ new->next = (*head);
+ (*head) = new;
+
+ return (*head);
 }
+
